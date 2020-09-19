81a3af9302d553acfee9cc7c02c62c6f29200236
Bartosz Szopiera <bartosz.szopiera@gmail.com>
2020-09-19 18:11:36 +0200

    Add ommited files.

---
+++ src/components/Toggle.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    {{ label }}
+    <label class="switch">
+      <input type="checkbox" :value="value" @input="$emit('checked', $event.target.checked)">
+      <span class="slider round"></span>
+    </label>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {
+    label: String,
+    value: Boolean,
+  },
+})
+export default class Toggle extends Vue {}
+</script>
+
+<style scoped lang="scss">
+  @import '@/style/toggle.scss'
+</style>
